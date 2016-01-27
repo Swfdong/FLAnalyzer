6c28efad350ba4c3f4938b2ea01b5ceb0d350cf3
Sheldon Zhang <swfdong@gmail.com>
2016-01-27 14:16:18 +0800

    工具方法迁移
    1.将原converter.js的方法迁移至normalizer.js；
    2.converter.js现用于数据的转换，如赔率转概率，赔率转赔付率等。

--- app/configs/spider.js
+++ app/configs/spider.js
@@ -2,16 +2,6 @@ module.exports = {
   //first_date: '2015-12-22',
   first_date: '2009-11-17',
   dict:{
-    OUZHI: [
-      {id:'0',name:'average'},
-      {id:'3',name:'bet365'},
-      {id:'5',name:'macau'},
-      {id:'293',name:'william'}
-    ],
-    YAPAN: [
-      {id:'3',name:'bet365'},
-      {id:'5',name:'macau'}
-    ],
     JINGCAI: {
       'nspf':'spf',
       'spf':'rqspf'
